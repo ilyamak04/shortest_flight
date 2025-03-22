9512e924e02f284d7b833bcbefe2961216864735
im.makarov <makilya04@gmail.com>
2025-03-22 22:28:36 +0300

    ADD stubs for compiling project

--- src/flight_graph.cpp
+++ src/flight_graph.cpp
@@ -10,3 +10,18 @@ void FlightGraph::add_edge(const std::string& origin,
 	graph[origin].push_back({ destination, distance });
 	graph[destination].push_back({ origin, distance }); 
 }
+
+std::pair<int, std::vector<std::string>> FlightGraph::find_shortest_path(const std::string& start, const std::string& end) 
+{
+	// Заглушка
+	return { 0, {start, end} };
+}
+
+std::string FlightGraph::get_min_distance_node(
+	const std::unordered_map<std::string, int>& distances,
+	const std::unordered_map<std::string, bool>& visited
+)
+{	
+	// Заглушка
+	return "";
+}
