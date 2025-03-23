49afca6487cad8398f5b87ef5144f49bebce5d1f
im.makarov <makilya04@gmail.com>
2025-03-23 14:22:55 +0300

    ADD logger in main func

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
-#include <iostream>
 #include "flight_graph.hpp" 
+#include "logger.hpp"
 
 int main() {
-    std::cout << "This is the flight project.\n";
+
+    Logger& logger = Logger::get_instance();
+    logger.initialize("../logs/server.log");
+
+    try {
+        logger.log(LogLevel::INFO, "main", "Program started!");
+        
+        // Лоигика
+    
+    } catch (const std::exception& e) {
+        logger.log(LogLevel::ERROR, "main",
+            std::string("Critical ERROR: ") + e.what());
+        return 1;
+    }
+
+    logger.log(LogLevel::INFO, "main", "Program has ended");
     return 0;
 }
